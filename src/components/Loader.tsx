2a553eb3bd6fd67d78bde4d31487e491117f7492
Danil Filimonov <lsamba99@gmail.com>
2021-11-15 19:27:39 +0700

    создаю компонент Loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+
+const Loader: React.FC = () => {
+  return (
+    <svg
+      style={{ margin: 'auto', background: 'rgb(255, 255, 255)', display: 'block', shapeRendering: 'auto' }}
+      width='200px'
+      height='200px'
+      viewBox='0 0 100 100'
+      preserveAspectRatio='xMidYMid'
+    >
+      <g transform='translate(50 50)'>
+        <g transform='scale(1)'>
+          <g transform='translate(-50 -50)'>
+            <g>
+              <animateTransform
+                attributeName='transform'
+                type='rotate'
+                repeatCount='indefinite'
+                values='0 50 50;360 50 50'
+                keyTimes='0;1'
+                dur='0.7575757575757576s'
+              ></animateTransform>
+              <path fillOpacity='1' fill='#85a2b6' d='M50 50L50 0A50 50 0 0 1 100 50Z'></path>
+            </g>
+            <g>
+              <animateTransform
+                attributeName='transform'
+                type='rotate'
+                repeatCount='indefinite'
+                values='0 50 50;360 50 50'
+                keyTimes='0;1'
+                dur='1.0101010101010102s'
+              ></animateTransform>
+              <path fillOpacity='1' fill='#bbcedd' d='M50 50L50 0A50 50 0 0 1 100 50Z' transform='rotate(90 50 50)'></path>
+            </g>
+            <g>
+              <animateTransform
+                attributeName='transform'
+                type='rotate'
+                repeatCount='indefinite'
+                values='0 50 50;360 50 50'
+                keyTimes='0;1'
+                dur='1.5151515151515151s'
+              ></animateTransform>
+              <path fillOpacity='1' fill='#dce4eb' d='M50 50L50 0A50 50 0 0 1 100 50Z' transform='rotate(180 50 50)'></path>
+            </g>
+            <g>
+              <animateTransform
+                attributeName='transform'
+                type='rotate'
+                repeatCount='indefinite'
+                values='0 50 50;360 50 50'
+                keyTimes='0;1'
+                dur='3.0303030303030303s'
+              ></animateTransform>
+              <path fillOpacity='1' fill='#fdfdfd' d='M50 50L50 0A50 50 0 0 1 100 50Z' transform='rotate(270 50 50)'></path>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  )
+}
+
+export default Loader
